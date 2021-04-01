16f8d4f616116d0a865841882bf78fa165d7a981
Sam Bennett <samuelhb201@gmail.com>
2021-03-31 17:12:17 -0700

    Author updated to Sam Bennett, Date changed to 3.31.2021

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Sam Bennett
+# Date: 3/31/2021
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
